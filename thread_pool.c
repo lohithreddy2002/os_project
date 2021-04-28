7ccc9ab71b9ff4058e1fe0d82bb4dfbcb45205fc
lohithreddy2002 <rocklohithreddy@gmail.com>
2021-04-28 09:22:40 +0530

    added comments

--- thread_pool.c
+++ thread_pool.c
@@ -4,54 +4,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define THREAD_NUM 1
+#define THREAD_NUM 1    //number of threads 
 
-typedef struct Task {
+typedef struct Task {       //task to be submiteed by the thread
     void (*func)(int,int);
     int arg1,arg2;
 } Task;
 
-Task taskQueue[256];
-int taskCount = 0;
+Task taskQueue[250];     //number of tasks in the queue
+int taskCount = 0;       //count of the present task
 
-pthread_mutex_t mutexQueue;
-pthread_cond_t condQueue;
+pthread_mutex_t mutexQueue;     // mutex
+pthread_cond_t condQueue;       // conditional variable
 
-void executeTask(Task* task) {
-    task -> func(task->arg1,task->arg2);
+void executeTask(Task* task) {                     // function that executes task in the thread
+    task -> func(task->arg1,task->arg2);   
     // int result = task->arg1 + task->arg2;
     // printf("hai");
 }
 
-void submitTask(Task task) {
-    pthread_mutex_lock(&mutexQueue);
-    taskQueue[taskCount] = task;
-    taskCount++;
-    pthread_mutex_unlock(&mutexQueue);
-    pthread_cond_signal(&condQueue);
+void submitTask(Task task) {                     //function that submits the task to the queue
+    pthread_mutex_lock(&mutexQueue);             //mutex acquire lock
+    taskQueue[taskCount] = task;                 // put the task in the task queue
+    taskCount++;                                 // increase the count variable
+    pthread_mutex_unlock(&mutexQueue);           // muntex release lock
+    pthread_cond_signal(&condQueue);             // conditional signal pointing to conditional wait in start thread
 }
 
 
 
-void* startThread(void* args) {
-    while (1) {
+void* startThread(void* args) {                         //function stating the thread
+    while (1) {  
         Task task;
-        pthread_mutex_lock(&mutexQueue);
-        while (taskCount == 0) {
-           pthread_cond_wait(&condQueue, &mutexQueue);
+        pthread_mutex_lock(&mutexQueue);              //mutex acquire 
+        while (taskCount == 0) {                          
+           pthread_cond_wait(&condQueue, &mutexQueue); //conditional wait 
         }
-        task = taskQueue[0];
+        task = taskQueue[0];      
         int i;
-        for (i = 0; i < taskCount - 1; i++) {
+        for (i = 0; i < taskCount - 1; i++) {        //push all the elements in the queue to the left
             taskQueue[i] = taskQueue[i + 1];
         }
-        taskCount--;
-        pthread_mutex_unlock(&mutexQueue);
-        executeTask(&task);
+        taskCount--;                                 //decrease the taskcount
+        pthread_mutex_unlock(&mutexQueue);           //mutex release
+        executeTask(&task);                          // calling execute task function
     }
 }
 
-
+//function we want to execute on the thread
 void sumfun(int q,int w){
     int sum = q+w;
     printf("number is thread %d,%d and the sum is %d\n",q,w,sum);
@@ -62,19 +62,20 @@ void product(int q,int w){
     printf("number is thread %d,%d and the product is %d\n",q,w,product);
     }
 
+//end
 
 int main(int argc, char* argv[]) {
     pthread_t th[THREAD_NUM];
-    pthread_mutex_init(&mutexQueue, NULL);
-    pthread_cond_init(&condQueue, NULL);
+    pthread_mutex_init(&mutexQueue, NULL);          //intitalize mutex
+    pthread_cond_init(&condQueue, NULL);            //initialize coditional variable
     int i;
     for (i = 0; i < THREAD_NUM; i++) {
-        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0) {
+        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0) {    //creating threads and checking if created
             perror("Failed to create the thread");
         }
     }
     
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 50; i++) {
         Task t = {
             .func = &sumfun,
             .arg1 = rand() % 100,
@@ -83,14 +84,7 @@ int main(int argc, char* argv[]) {
         submitTask(t);
     }
 
-    for (i = 0; i < 5; i++) {
-        Task t = {
-            .func = &product,
-            .arg1 = rand() % 100,
-            .arg2  = rand() % 100
-        };
-        submitTask(t);
-    }
+
     
 
 
